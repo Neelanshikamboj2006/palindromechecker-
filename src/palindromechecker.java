7db081111890e1ab3420e56a3c2cca4d05518153
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-26 09:18:26 +0530

    UC2:Handcoded palindrome result

--- src/palindromechecker.java
+++ src/palindromechecker.java
@@ -1,7 +1,14 @@
-public class palindromechecker{
-    public static void main(String[] args){
-        System.out.println("Welcome to the palindrome checker:");
-        System.out.println("Version:");
-        System.out.println("System initalized successfully");
+public class palindromechecker {
+    public static void main(String[] args) {
+        String word = "madam";
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+        if (word.equals(reversed)) {
+            System.out.println("The given string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
+        }
     }
 }
