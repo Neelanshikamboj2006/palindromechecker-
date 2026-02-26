b7c548f30635215173d04cbffd63e5882e6bbd79
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-26 11:00:19 +0530

    UC12:Stratergy pattern palindrome

--- src/palindromechecker.java
+++ src/palindromechecker.java
@@ -1,23 +1,51 @@
-
-class PalindromeChecker {
+import java.util.*;
+interface PalindromeStrategy {
+    boolean checkPalindrome(String word);
+}
+class StackStrategy implements PalindromeStrategy {
     public boolean checkPalindrome(String word) {
-        int start = 0;
-        int end = word.length() - 1;
-        while (start < end) {
-            if (word.charAt(start) != word.charAt(end)) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
         return true;
     }
 }
-public class palindromechecker {
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+class PalindromeService {
+    private PalindromeStrategy strategy;
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+    public boolean execute(String word) {
+        return strategy.checkPalindrome(word);
+    }
+}
+public class palindromechecker{
     public static void main(String[] args) {
-        String input = "level";
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.checkPalindrome(input);
+        String input = "madam";
+        PalindromeStrategy strategy = new StackStrategy();
+        PalindromeService service = new PalindromeService(strategy);
+        boolean result = service.execute(input);
         if (result) {
             System.out.println("The given string \"" + input + "\" is a Palindrome.");
         } else {
