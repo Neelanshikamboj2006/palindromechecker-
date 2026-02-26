93f2f14e1a77ef921535a56b9a664b3e70971f31
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-26 09:02:30 +0530

    UC1:Add welcome message module

--- src/palindromechecker.java
+++ src/palindromechecker.java
@@ -1,2 +1,7 @@
-public class palindromechecker {
+public class palindromechecker{
+    public static void main(String[] args){
+        System.out.println("Welcome to the palindrome checker:");
+        System.out.println("Version:");
+        System.out.println("System initalized successfully");
+    }
 }
