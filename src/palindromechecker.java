250b871966eea356d485445836b92827016b93c5
Neelanshikamboj2006 <neelanshikamboj@gmail.com>
2026-02-26 10:43:29 +0530

    UC10:Insensitive and space ignored palindrome

--- src/palindromechecker.java
+++ src/palindromechecker.java
@@ -1,20 +1,22 @@
 public class palindromechecker{
-    public static boolean isPalindrome(String word, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-        if (word.charAt(start) != word.charAt(end)) {
-            return false;
-        }
-        return isPalindrome(word, start + 1, end - 1);
-    }
     public static void main(String[] args) {
-        String word = "racecar";
-        boolean result = isPalindrome(word, 0, word.length() - 1);
-        if (result) {
-            System.out.println("The given string \"" + word + "\" is a Palindrome.");
+        String input = "Never Odd Or Even";
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        int start = 0;
+        int end = normalized.length() - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("The given string \"" + input + "\" is a Palindrome (ignoring case and spaces).");
         } else {
-            System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
+            System.out.println("The given string \"" + input + "\" is NOT a Palindrome.");
         }
     }
 }
